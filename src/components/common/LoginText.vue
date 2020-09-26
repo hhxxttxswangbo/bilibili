ffd8c90f19b06755b990db605fdeda6d2e11b184
晚安北京 <120907030@qq.com>
2020-09-26 10:14:53 +0800

    封装了登录注册输入框

---
+++ src/components/common/LoginText.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <van-field
+      :label="label"
+      :type="type"
+      :placeholder="placeholder"
+      :rule="rule"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["label", "type", "placeholder", "rule"],
+};
+</script>
+
+<style>
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <!-- middleTop接收LoginTop传过来的值 -->
+    <!-- middleTop接收LoginTop传过来的值，由于是个字符串不加冒号 -->
     <login-top middleTop="注册bilibili"></login-top>
+    <login-text
+      label="姓名"
+      style="margin: 15px 0"
+      placeholder="请输入姓名"
+      rule="^.{6,16}$"
+    ></login-text>
+    <login-text
+      label="账号"
+      placeholder="请输入账号"
+      rule="^.{6,16}$"
+    ></login-text>
+    <login-text
+      label="密码"
+      placeholder="请输入密码"
+      type="password"
+      rule="^.{6,16}$"
+    ></login-text>
   </div>
 </template>
 
 <script>
 import LoginTop from "@/components/common/LoginTop.vue";
+import LoginText from "@/components/common/LoginText.vue";
 export default {
   components: {
     LoginTop,
+    LoginText,
   },
 };
 </script>
